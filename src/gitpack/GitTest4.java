04e3b518a31839b9cde16810a9fa65b952e81e14
lkarlapalem <lkarlapalem@OSIT-L-0481.osius.com>
2022-04-15 12:41:34 +0530

    c5

--- src/gitpack/GitTest4.java
+++ src/gitpack/GitTest4.java
@@ -4,6 +4,9 @@ public class GitTest4 {
 
 	public static void main(String[] args) {
 		System.out.println("git test 4444");
+		System.out.println("git test 1111");
+		System.out.println("git test 2222");
+		System.out.println("git test 3333");
 	}
 
 }
